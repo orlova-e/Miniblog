385693aedb07b866dd29b2d8fa3440b600ea96a8
Elizaveta Orlova <orlovaev11@gmail.com>
2020-08-17 18:03:40 +0400

    template v3

---
+++ js/scrollFunction.js
@@ -0,0 +1,7 @@
+window.onscroll = function() { scrollPageFunction() };
+function scrollPageFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("readBar").style.width = scrolled + "%";
+}
