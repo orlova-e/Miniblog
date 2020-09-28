2c954c1c5c2226ced4fab6606e441b4e191e6bba
Elizaveta Orlova <orlovaev11@gmail.com>
2020-09-29 00:17:45 +0400

    Basic layout (css) is improved. selectTheme.js is modified (element id is changed)

--- Miniblog/wwwroot/js/add_article_actions/selectTheme.js
+++ Miniblog/wwwroot/js/add_article_actions/selectTheme.js
@@ -1,12 +1,14 @@
 "use strict";
 
 function selectTheme(btn) {
-    let themeSwitcher = document.getElementById("ColorTheme");
-    themeSwitcher.setAttribute("value", btn.id);
+    let themeSwitcher = document.getElementById("DisplayOptions_ColorTheme");
+    let val = String(btn.dataset.themeValue);
+    themeSwitcher.setAttribute("value", val);
+
     let circles = btn.closest("div.article-circles");
     let circlesCollection = circles.querySelectorAll("button");
-    for(let i = 0; i < circlesCollection.length; i++) {
-        if(circlesCollection[i].classList.contains("selected-circle"))
+    for (let i = 0; i < circlesCollection.length; i++) {
+        if (circlesCollection[i].classList.contains("selected-circle"))
             circlesCollection[i].classList.remove("selected-circle");
     }
     btn.classList.add("selected-circle");
