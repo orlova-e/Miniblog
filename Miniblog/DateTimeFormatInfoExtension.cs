de6df5aaa3667fc6646d1ae52b43cef09811bd17
Elizaveta Orlova <orlovaev11@gmail.com>
2020-11-03 00:25:57 +0400

    roundtrip datetime format pattern extension is added

---
+++ Miniblog/DateTimeFormatInfoExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Miniblog
+{
+    public static class DateTimeFormatInfoExtension
+    {
+        public static string RoundtripDtPattern(this DateTimeFormatInfo dateTimeFormatInfo)
+        {
+            return "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffK";
+        }
+        public static string RoundtripDtOffsetPattern(this DateTimeFormatInfo dateTimeFormatInfo)
+        {
+            return "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffzzz";
+        }
+    }
+}
