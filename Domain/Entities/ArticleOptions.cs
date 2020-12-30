edf93a0fa7a786a4982003dddcae2c7d3b0ec4ce
Elizaveta Orlova <orlovaev11@gmail.com>
2020-12-30 17:18:56 +0300

    Id and Discriminator properties returned to the BaseDisplayOptions. Now they have an JsonIgnore attribute

--- Domain/Entities/ArticleOptions.cs
+++ Domain/Entities/ArticleOptions.cs
@@ -1,14 +1,10 @@
 ﻿using Domain.Entities.Enums;
 using System;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
     public class ArticleOptions : BaseDisplayOptions
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid Id { get; set; }
-        public string Discriminator { get; }
         public Guid ArticleId { get; set; }
         public Article Article { get; set; }
         public ColorTheme ColorTheme { get; set; }
--- Domain/Entities/BaseDisplayOptions.cs
+++ Domain/Entities/BaseDisplayOptions.cs
@@ -1,9 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Domain.Entities
 {
     public abstract class BaseDisplayOptions
     {
+        [JsonIgnore]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [JsonIgnore]
+        public string Discriminator { get; }
         public bool Username { get; set; }
         [Display(Name = "Date and time")]
         public bool DateAndTime { get; set; }
