d0e2ef3aba098c9185c6b11b883fb417de46231c
Elizaveta Orlova <orlovaev11@gmail.com>
2021-01-18 23:12:17 +0300

    add: the FoundObject<T> class

---
+++ Services/FoundValues/FoundObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services.FoundValues
+{
+    public class FoundObject<T>
+        where T : class, new()
+    {
+        public int TotalRating { get; set; }
+        public List<string> MatchedWords { get; set; }
+        public Guid EntityId { get; set; }
+        public T Entity { get; set; }
+    }
+}
