e23b88babd75dd11d968a7592952102586d6453f
Elizaveta Orlova <orlovaev11@gmail.com>
2021-01-23 12:19:24 +0300

    The ability to use the path to the site icon from the configuration file has been added
    * It requires the IconTagHelper class
    * <icon/> is now used in the _Layout.cshtml
    * tag-helper's namespace was added to the _ViewImports.cshtml file

---
+++ Miniblog/Infrastructure/TagHelpers/IconTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Options;
+using System.IO;
+using Web.Configuration;
+
+namespace Web.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("icon", ParentTag = "head", TagStructure = TagStructure.WithoutEndTag)]
+    public class IconTagHelper : TagHelper
+    {
+        [HtmlAttributeNotBound]
+        public string IconPath { get; set; }
+        public IconTagHelper(IOptionsSnapshot<BlogOptions> optionsSnapshot)
+        {
+            IconPath = optionsSnapshot.Value.WebsiteOptions.IconPath;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "link";
+            output.TagMode = TagMode.StartTagOnly;
+            output.Attributes.SetAttribute("rel", "shortcut icon");
+            output.Attributes.SetAttribute("href", Path.Combine("~", IconPath));
+        }
+    }
+}
