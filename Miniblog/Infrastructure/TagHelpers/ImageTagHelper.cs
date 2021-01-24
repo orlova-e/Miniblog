21bf3e3c1120a6c8919c5b2bf05e62ae415a31c1
Elizaveta Orlova <orlovaev11@gmail.com>
2021-01-24 23:26:19 +0300

    The ImageTagHelper class was added

---
+++ Miniblog/Infrastructure/TagHelpers/ImageTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Web.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("image", TagStructure = TagStructure.WithoutEndTag)]
+    public class ImageTagHelper : TagHelper
+    {
+        public Image Source { get; set; }
+        public int HeightPx { get; set; }
+        public int WidthPx { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+            output.TagMode = TagMode.StartTagOnly;
+
+            if (Source == null || Source.File.Length == 0)
+                return;
+
+            output.Attributes.SetAttribute("src", "data:image/jpeg;base64," + Convert.ToBase64String(Source.File));
+            output.Attributes.SetAttribute("alt", Source.Name);
+
+            if (HeightPx > 0)
+            {
+                output.Attributes.SetAttribute("height", HeightPx + "px");
+            }
+            if (WidthPx > 0)
+            {
+                output.Attributes.SetAttribute("width", WidthPx + "px");
+            }
+        }
+    }
+}
