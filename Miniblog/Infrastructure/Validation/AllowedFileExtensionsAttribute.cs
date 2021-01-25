62c6b0d3bad8a38acf99f713aa0d57c60acbcad6
Elizaveta Orlova <orlovaev11@gmail.com>
2021-01-25 11:32:16 +0300

    The AllowedFileExtensions and the MaxFileSize validation attributes were added

---
+++ Miniblog/Infrastructure/Validation/AllowedFileExtensionsAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace Web.Infrastructure.Validation
+{
+    public class AllowedFileExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string[] extensions;
+        public AllowedFileExtensionsAttribute(params string[] extensions)
+        {
+            if (extensions?.Any() ?? default)
+                this.extensions = extensions;
+            else
+                this.extensions = new string[] { };
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile formFile)
+            {
+                string fileExtension = Path.GetExtension(formFile.FileName).TrimStart('.');
+                if (extensions.Any() && !extensions.Contains(fileExtension))
+                {
+                    string errorMessage = !string.IsNullOrWhiteSpace(ErrorMessage) ? ErrorMessage : "This file extension is not allowed";
+                    return new ValidationResult(errorMessage);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Miniblog/Infrastructure/Validation/MaxFileSizeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Infrastructure.Validation
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly long maxSize;
+        public MaxFileSizeAttribute(long maxFileSize)
+        {
+            maxSize = maxFileSize;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is IFormFile formFile)
+            {
+                if (formFile.Length > maxSize)
+                {
+                    string errorMessage = !string.IsNullOrWhiteSpace(ErrorMessage) ? ErrorMessage : $"Maximum allowed file size is {maxSize / 1024 / 1024} MB";
+                    return new ValidationResult(errorMessage);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
