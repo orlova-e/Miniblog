cf160c3946c6bc59785b220051c5118c40c425bc
Elizaveta Orlova <orlovaev11@gmail.com>
2021-01-30 20:53:07 +0300

    The TitleTagHelper usage in _Layout.cshtml has been added

---
+++ Miniblog/Infrastructure/TagHelpers/TitleTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Options;
+using Web.Configuration;
+
+namespace Web.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("title", Attributes = "page", ParentTag = "head")]
+    public class TitleTagHelper : TagHelper
+    {
+        public string Page { get; set; }
+        [HtmlAttributeNotBound]
+        public WebsiteOptions WebsiteOptions { get; private set; }
+        public TitleTagHelper(IOptionsSnapshot<BlogOptions> optionsSnapshot)
+        {
+            WebsiteOptions = optionsSnapshot.Value.WebsiteOptions;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string title = WebsiteOptions.Name;
+
+            if (!string.IsNullOrWhiteSpace(Page))
+            {
+                title = Page + " — " + title;
+            }
+            else if (!string.IsNullOrWhiteSpace(WebsiteOptions.Subtitle))
+            {
+                title += " — " + WebsiteOptions.Subtitle;
+            }
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Content.SetContent(title);
+        }
+    }
+}
