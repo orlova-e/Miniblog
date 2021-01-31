c4bb601ae5c7439b536574b700b0fd9dc26e09aa
Elizaveta Orlova <orlovaev11@gmail.com>
2021-01-31 20:46:39 +0300

    Extensions that allows to read properties/enums' fields display names were added

---
+++ Miniblog/Infrastructure/Extensions/DisplayAttributeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Web.Infrastructure.Extensions
+{
+    public static class DisplayAttributeExtensions
+    {
+        public static string GetDisplayName(this object obj, string propertyName)
+        {
+            Type type = obj.GetType();
+            MemberInfo property = type.GetProperty(propertyName);
+            if (property.GetCustomAttribute(typeof(DisplayAttribute)) is DisplayAttribute displayAttribute)
+            {
+                return displayAttribute.Name;
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Miniblog/Infrastructure/Extensions/EnumExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Web.Infrastructure.Extensions
+{
+    public static class EnumExt
+    {
+        public static string GetEnumDisplayName(Type enumType, object value)
+        {
+            if (!enumType.IsEnum)
+                throw new ArgumentException();
+
+            string name = Enum.GetName(enumType, value);
+            FieldInfo property = enumType.GetField(name);
+            if (property.GetCustomAttribute(typeof(DisplayAttribute)) is DisplayAttribute displayAttribute)
+            {
+                return displayAttribute.Name;
+            }
+            return string.Empty;
+        }
+    }
+}
