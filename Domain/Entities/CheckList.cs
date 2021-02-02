9c95fc6dd048154ceb6eb7283ca7d9c4b066cc38
Elizaveta Orlova <orlovaev11@gmail.com>
2021-02-02 23:16:49 +0300

    The CheckList class and the CheckAction enum were added

---
+++ Domain/Entities/CheckList.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities.Enums;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace Domain.Entities
+{
+    public class CheckList : IEnumerable<string>
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string VerifiableWords { get; set; }
+        public CheckAction CheckAction { get; set; }
+        protected IEnumerable<string> words = new List<string>();
+        public IEnumerator<string> GetEnumerator()
+        {
+            if (!words.Any() && !string.IsNullOrWhiteSpace(VerifiableWords))
+                words = VerifiableWords.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            return words.GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            if (!words.Any() && !string.IsNullOrWhiteSpace(VerifiableWords))
+                words = VerifiableWords.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            return words.GetEnumerator();
+        }
+    }
+}
---
+++ Domain/Entities/Enums/CheckAction.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities.Enums
+{
+    public enum CheckAction
+    {
+        Verify,
+        Delete
+    }
+}
