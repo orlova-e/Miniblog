fc67c60d07b8529b2b31a8462ee9945cb47a8dbd
Elizaveta Orlova <orlovaev11@gmail.com>
2021-02-02 23:22:32 +0300

    The /options/checklists was added

---
+++ Miniblog/ViewModels/Options/CheckListViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.ViewModels.Options
+{
+    public class CheckListViewModel
+    {
+        public string ListToCheck { get; set; }
+        public string BlackList { get; set; }
+    }
+}
