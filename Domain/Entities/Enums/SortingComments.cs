bdd14af35b670a97ac87743bb515f91a1b35ee53
Elizaveta Orlova <orlovaev11@gmail.com>
2021-02-06 20:18:03 +0300

    fix(SortingComments.cs): display attributes' were added

--- Domain/Entities/Enums/SortingComments.cs
+++ Domain/Entities/Enums/SortingComments.cs
@@ -1,9 +1,14 @@
-﻿namespace Domain.Entities.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Enums
 {
     public enum SortingComments
     {
+        [Display(Name = "Most liked")]
         MostLiked,
+        [Display(Name = "New first")]
         NewFirst,
+        [Display(Name = "Old first")]
         OldFirst
     }
 }
