5fe7a3e16dbed069d5494e5b753635560edf878d
Elizaveta Orlova <orlovaev11@gmail.com>
2021-02-07 20:11:56 +0300

    fix(Discussion.cshtml): using new style for labels

--- Miniblog/Configuration/CommentsOptions.cs
+++ Miniblog/Configuration/CommentsOptions.cs
@@ -1,9 +1,11 @@
 ﻿using Domain.Entities.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Configuration
 {
     public class CommentsOptions
     {
+        [Display(Name = "Allow nesting")]
         public bool AllowNesting { get; set; }
         public Changeable Depth { get; set; }
         public SortingComments SortingCommentsDefaultType { get; set; }
