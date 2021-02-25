27f3e5f9c602d739e957e90b263c01309045f377
Elizaveta Orlova <orlovaev11@gmail.com>
2021-02-25 23:12:27 +0300

    fix(AccessAttribute): the code has been simplified

--- Miniblog/Filters/AccessAttribute.cs
+++ Miniblog/Filters/AccessAttribute.cs
@@ -1,51 +1,49 @@
 ﻿using Domain.Entities;
-using Domain.Entities.Enums;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Repo.Interfaces;
+using Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Security.Claims;
 
 namespace Web.Filters
 {
     public class AccessAttribute : IAuthorizationFilter
     {
-        public IOptionRepository<Role> RolesRepository { get; private set; }
-        private List<string> Requirements { get; }
-        public AccessAttribute(IOptionRepository<Role> rolesRepository, string requirements)
+        private IUserService UserService { get; }
+        private IEnumerable<string> Requirements { get; }
+        public AccessAttribute(IUserService userService, string requirements)
         {
-            RolesRepository = rolesRepository;
-            //_requirements = requirements.ToList();
-            Requirements = requirements.Split(",").ToList();
-            for (int i = 0; i < Requirements.Count(); i++)
-                Requirements[i] = Requirements[i].Trim();
-            PropertyInfo[] propertyInfos = (from property in typeof(ExtendedRole).GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                            where property.PropertyType == typeof(bool)
-                                            select property).ToArray();
-            string[] propertyNames = new string[propertyInfos.Length];
-            for (int i = 0; i < propertyNames.Length; i++)
-                propertyNames[i] = propertyInfos[i].Name;
-            Requirements = Requirements.Intersect(propertyNames, StringComparer.OrdinalIgnoreCase).ToList();
+            UserService = userService;
+            Requirements = requirements.Split(",").Select(r => r.Trim());
+
+            IEnumerable<string> propertyNames = typeof(ExtendedRole)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(p => p.PropertyType == typeof(bool))
+                .Select(p => p.Name);
+
+            Requirements = Requirements.Intersect(propertyNames, StringComparer.OrdinalIgnoreCase);
         }
         public void OnAuthorization(AuthorizationFilterContext context)
         {
             if (context.HttpContext.User.Identity.IsAuthenticated)
             {
-                string roleType = context.HttpContext.User.FindFirstValue(ClaimsIdentity.DefaultRoleClaimType);
-                RoleType type = (RoleType)Enum.Parse(typeof(RoleType), roleType);
-                Role role = RolesRepository.Find(r => r.Type == type).FirstOrDefault();
-                PropertyInfo[] propertyInfos = (from property in role.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                                where property.PropertyType == typeof(bool)
-                                                select property).ToArray();
+                Role role = UserService.FindByName(context.HttpContext.User.Identity.Name).Role;
+                PropertyInfo[] propertyInfos = role.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
                 List<string> allowedActions = new List<string>();
                 foreach (var property in propertyInfos)
+                {
                     if ((bool)property.GetValue(role))
+                    {
                         allowedActions.Add(property.Name);
+                    }
+                }
+
                 var compared = Requirements.Except(allowedActions);
+
                 if (compared.Any())
                     context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
             }
