334af1bb34577cee17982c0ab818cefd74167965
Elizaveta Orlova <orlovaev11@gmail.com>
2021-03-27 00:15:41 +0300

    Fix lists' pages client code, unnecessary files have been removed

--- Miniblog/wwwroot/js/list/articlesLayout.js
+++ Miniblog/wwwroot/js/list/articlesLayout.js
@@ -34,3 +34,16 @@ function articlesLayoutVisibility(btn) {
             localStorage.setItem(layoutType, "Grid");
     }
 }
+
+let articlesLayoutBtn;
+if (localStorage.getItem(layoutType) !== null) {
+    let id = localStorage.getItem(layoutType) + "ArticlesLayout";
+    articlesLayoutBtn = document.getElementById(id);
+} else {
+    let displayButtons = document.querySelector(".display-character");
+    localStorage.setItem(layoutType, displayButtons.dataset.btnActive);
+    let id = displayButtons.dataset.btnActive + "ArticlesLayout";
+    articlesLayoutBtn = document.getElementById(id);
+}
+
+window.addEventListener("load", () => articlesLayoutVisibility(articlesLayoutBtn));
