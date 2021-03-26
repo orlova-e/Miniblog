28c2fcc49340033d222a672452962d6f4edbdb8c
Elizaveta Orlova <orlovaev11@gmail.com>
2021-03-26 23:32:37 +0300

    The LoginViewModel's validation has been improved

--- Miniblog/ViewModels/LoginViewModel.cs
+++ Miniblog/ViewModels/LoginViewModel.cs
@@ -6,10 +6,13 @@ namespace Web.ViewModels
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Username is required")]
+        [MinLength(4, ErrorMessage = "The minimum length is 4 characters")]
+        [MaxLength(25, ErrorMessage = "The maximum length is 25 characters")]
         [DataType(DataType.Text)]
         public string Username { get; set; }
         [Required(ErrorMessage = "Password is required")]
         [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "The minimum length is 8 characters")]
         public string Password { get; set; }
 
         public static explicit operator Account(LoginViewModel loginViewModel) => new Account
