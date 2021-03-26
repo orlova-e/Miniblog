c2b5c087ae5fe375c8f12d1888726cffb752c9ef
Elizaveta Orlova <orlovaev11@gmail.com>
2021-03-27 00:00:49 +0300

    fix articlesLayoutVisibility.js for using with UIKit's grid classes

--- Miniblog/wwwroot/js/listpage_actions/articlesLayoutVisibility.js
+++ Miniblog/wwwroot/js/listpage_actions/articlesLayoutVisibility.js
@@ -1,24 +1,35 @@
 "use strict";
+
 const layoutType = "ArticlesLayoutType";
 function articlesLayoutVisibility(btn) {    
     let btnImg = btn.querySelector('img');
-    let blogContainer = document.getElementById("articlesContainer");
+    let container = document.getElementById("articlesContainer");
 
     if (btn.id === "RowsArticlesLayout") {
-        btnImg.src = "../img/ico/rows.png";
+        btnImg.src = "/img/buttons/rows.png";
         let anotherBtn = document.getElementById("GridArticlesLayout");
         let anotherBtnImg = anotherBtn.querySelector('img');
-        anotherBtnImg.src = "../img/ico/grid_nonactive.png";
-        blogContainer.style.gridTemplateColumns = "1fr";
+        anotherBtnImg.src = "/img/buttons/grid_nonactive.png";
+
+        if (container.classList.contains("uk-child-width-1-2@m")) {
+            container.classList.remove("uk-child-width-1-2@m");
+        }
+        container.classList.add("uk-child-width-1-1@m");
+
         if (localStorage.getItem(layoutType) !== "Rows")
             localStorage.setItem(layoutType, "Rows");
     }
     else if (btn.id === "GridArticlesLayout") {
-        btnImg.src = "../img/ico/grid.png";
+        btnImg.src = "/img/buttons/grid.png";
         let anotherBtn = document.getElementById("RowsArticlesLayout");
         let anotherBtnImg = anotherBtn.querySelector('img');
-        anotherBtnImg.src = "../img/ico/rows_nonactive.png";
-        blogContainer.style.gridTemplateColumns = "1fr 1fr";
+        anotherBtnImg.src = "/img/buttons/rows_nonactive.png";
+
+        if (container.classList.contains("uk-child-width-1-1@m")) {
+            container.classList.remove("uk-child-width-1-1@m");
+        }
+        container.classList.add("uk-child-width-1-2@m");
+
         if(localStorage.getItem(layoutType) !== "Grid")
             localStorage.setItem(layoutType, "Grid");
     }
