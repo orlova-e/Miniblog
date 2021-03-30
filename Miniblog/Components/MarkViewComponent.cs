f51759500c6e582e97e6c627d009ffa378588f22
Elizaveta Orlova <orlovaev11@gmail.com>
2021-03-30 20:06:44 +0300

    The MarkViewComponent has been added (this is used to add the <mark> tag to the text)

---
+++ Miniblog/Components/MarkViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System.Collections.Generic;
+using System.Net;
+using System;
+
+namespace Web.Components
+{
+    public class MarkViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string text, List<string> matches)
+        {
+            text = WebUtility.HtmlEncode(text);
+
+            foreach (string match in matches)
+            {
+                if (!text.Contains(match, StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                int start = 0, current = 0;
+                do
+                {
+                    current = start;
+                    start = text.IndexOf(match, start, StringComparison.OrdinalIgnoreCase);
+                    text = text.Substring(0, start) + "<mark>" + text.Substring(start, match.Length) + "</mark>" + text.Substring(start + match.Length);
+                    start += "<mark></mark>".Length;
+                } while (start > current && text.IndexOf(match, start, StringComparison.OrdinalIgnoreCase) > -1);
+            }
+
+            IHtmlContent content = new HtmlString(text);
+            return new HtmlContentViewComponentResult(content);
+        }
+    }
+}
