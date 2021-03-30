33e15fe05b5d85f795140170105b9320fd9eba80
Elizaveta Orlova <orlovaev11@gmail.com>
2021-03-30 21:32:08 +0300

    Authors' and subscriptions' pages have been added. Routing to a specific author's page has been fixed

--- Miniblog/Controllers/UsersController.cs
+++ Miniblog/Controllers/UsersController.cs
@@ -1,8 +1,11 @@
 ﻿using Domain.Entities;
 using Domain.Entities.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Repo.Interfaces;
 using Services.Interfaces;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Web.App.Interfaces;
 using Web.ViewModels;
@@ -12,22 +15,25 @@ namespace Web.Controllers
     public class UsersController : Controller
     {
         private IArticleService ArticleService { get; set; }
+        private IRepository Repository { get; }
         private IListCreator ListCreator { get; set; }
         private IUserService UserService { get; set; }
         private ICommon Common { get; }
         public UsersController(IArticleService articleService,
+            IRepository repository,
             IListCreator listCreator,
             IUserService userService,
             ICommon common)
         {
             ArticleService = articleService;
+            Repository = repository;
             ListCreator = listCreator;
             UserService = userService;
             Common = common;
         }
 
         [HttpGet]
-        [Route("[controller]/{username}")]
+        [Route("[controller]/[action]/{username}")]
         public async Task<IActionResult> Account(string username, uint page = 1, ListSorting sortBy = ListSorting.NewFirst)
         {
             User author = UserService.GetUserFromDb(u => u.Username == username);
@@ -53,6 +59,33 @@ namespace Web.Controllers
             ViewBag.Subscribed = subscribed;
 
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Authors(uint page = 1)
+        {
+            var roles = (await Repository.Roles.GetAllAsync())
+                .Where(r => r.WriteArticles);
+            List<User> authors = (await Repository.Users.FindAsync(u => roles.Contains(u.Role) && u.Accepted is not false))?.ToList();
+            ListViewModel<User> listViewModel = new(page, authors, Common.Options.ListOptions);
+            listViewModel.PageName = "Authors";
+
+            if (page > 1 && !listViewModel.Entities.Any())
+                return NotFound();
+
+            return View(listViewModel);
+        }
+
+        [HttpGet, Authorize]
+        public async Task<IActionResult> Subscriptions(uint page = 1)
+        {
+            User user = UserService.FindByName(User.Identity.Name);
+            List<User> subscriptions = await Repository.Subscriptions.GetSubscriptionAsync(user.Id);
+            ListViewModel<User> listViewModel = new(page, subscriptions, Common.Options.ListOptions);
+            listViewModel.PageName = "Subscriptions";
+            listViewModel.ItemName = user.Username;
+
+            return View("Authors", listViewModel);
         }
     }
 }
