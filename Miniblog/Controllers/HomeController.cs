69c0086a3b6d1ea1738c9c47a0cd21e60fc2655a
Elizaveta Orlova <orlovaev11@gmail.com>
2021-03-30 20:43:05 +0300

    The search option has been added

--- Miniblog/Controllers/HomeController.cs
+++ Miniblog/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Repo.Interfaces;
+using Services.FoundValues;
 using Services.Interfaces;
+using Services.Interfaces.Search;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,18 +17,42 @@ namespace Web.Controllers
         public IRepository Repository { get; private set; }
         public IListCreator ListCreator { get; set; }
         public ICommon Common { get; set; }
+        public IAggregateSearch<Article> ArticlesSearch { get; set; }
+        public IAggregateSearch<User> UsersSearch { get; set; }
 
         public HomeController(IRepository repository,
             IListCreator listCreator,
-            ICommon common)
+            ICommon common,
+            IAggregateSearch<Article> articlesSearch,
+            IAggregateSearch<User> usersSearch)
         {
             Repository = repository;
             ListCreator = listCreator;
+            ArticlesSearch = articlesSearch;
+            UsersSearch = usersSearch;
             Common = common;
         }
         public IActionResult Index()
         {
             return RedirectToAction("Lists", "Articles");
+        }
+
+        public async Task<IActionResult> Search(string query, uint page = 1)
+        {
+            if (!Common.Options.WebsiteOptions.ShowSearchOption)
+                return NotFound();
+            List<FoundObject> items = new();
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                items = await ArticlesSearch.FindAsync(query);
+                items = items.Concat(await UsersSearch.FindAsync(query)).ToList();
+            }
+            ListViewModel<FoundObject> listViewModel = new(page, items, Common.Options.ListOptions);
+            listViewModel.PageName = "Search";
+            listViewModel.ItemName = query;
+            if (page > 1 && !listViewModel.Entities.Any())
+                return NotFound();
+            return View(listViewModel);
         }
 
         public async Task<IActionResult> Topics(uint page = 1)
@@ -72,7 +98,7 @@ namespace Web.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> SeriesWith(string name, uint page = 1)
+        public async Task<IActionResult> SeriesWith([FromQuery] string name, uint page = 1)
         {
             ListViewModel<Article> listViewModel;
             Series series = Repository.Series.Find(s => s.Link == name && s.Accepted is not false).FirstOrDefault();
