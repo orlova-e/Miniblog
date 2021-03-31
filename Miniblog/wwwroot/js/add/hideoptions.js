0d68b8c91c87b7f8f18050f3182c5b89f27b6a6a
Elizaveta Orlova <orlovaev11@gmail.com>
2021-03-31 07:07:38 +0300

    The add article view has been fixed: using properties' taghelpers and client code to hide display options and show labels

---
+++ Miniblog/wwwroot/js/add/hideoptions.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+function hideOptions(btn) {
+    let id = btn.dataset.targetId;
+    let options = document.getElementById(id);
+    let optionsLabel = btn.querySelector(`span.uk-icon`);
+    if (options.style.display === "none") {
+        options.style.display = "block";
+        optionsLabel.setAttribute("uk-icon", "icon: chevron-down");
+    } else {
+        options.style.display = "none";
+        optionsLabel.setAttribute("uk-icon", "icon: chevron-up");
+    }
+}
---
+++ Miniblog/wwwroot/js/add/showlabel.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+function showLabel(input) {
+    let label = document.querySelector(`label[for="${input.id}"]`);
+    if (input.value.length > 0) {
+        label.style.visibility = "visible";
+    } else {
+        label.style.visibility = "collapse";
+    }
+}
